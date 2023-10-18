be7eb81c11078634bdd02c83abcb2aff9585d261
SatananY <satananyindee@gmail.com>
2023-10-18 15:04:47 +0700

    add table

--- src/app/admin/reward-setting/reward-setting.component.ts
+++ src/app/admin/reward-setting/reward-setting.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reward-setting',
   templateUrl: './reward-setting.component.html',
-  styleUrls: ['./reward-setting.component.css']
+  styleUrls: ['./reward-setting.component.css'],
 })
 export class RewardSettingComponent {
+  showTable: boolean = false;
 
+  add() {
+    this.showTable = true;
+  }
+  clear() {
+    this.showTable = false;
+  }
 }
