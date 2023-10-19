e38a16b47ad9aa5f5a8f97e6ab1807575a248c7a
SatananY <satananyindee@gmail.com>
2023-10-19 13:50:51 +0700

    edit input reward-setting

--- src/app/admin/reward-setting/reward-setting.component.ts
+++ src/app/admin/reward-setting/reward-setting.component.ts
@@ -6,12 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./reward-setting.component.css'],
 })
 export class RewardSettingComponent {
-  showTable: boolean = false;
+  showTable: boolean = true;
+  date: string = '';
+  time: string = '';
+  rewardno: string = '';
+  rewardamount: string = '';
+  reward: string = '';
 
   add() {
     this.showTable = true;
   }
   clear() {
-    this.showTable = false;
+    this.showTable = true;
+    this.date = "";
+    this.time = "";
+    this.rewardno = "";
+    this.rewardamount = "";
+    this.reward = "";
   }
 }
