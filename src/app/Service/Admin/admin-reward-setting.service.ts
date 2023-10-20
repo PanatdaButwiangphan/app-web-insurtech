0a0e25524931ef765c9e3b46f2994140e814067e
sasithon <sasithon.koetthong@gmail.com>
2023-10-20 16:37:36 +0700

    api  addReward, showReward

---
+++ src/app/Service/Admin/admin-reward-setting.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class adminRewardSettingService {
+  constructor(private http: HttpClient) {}
+
+  addReward(reward: any) {
+    return this.http
+      .post<any>('http://94.74.114.51:7088/insurtech/api/v1/reward', reward)
+      .pipe((res: any) => res);
+  }
+  showReward() {
+    return this.http
+      .get<any>('http://94.74.114.51:7088/insurtech/api/v1/rewards')
+      .pipe((res: any) => res);
+  }
+}
--- src/app/admin/reward-setting/reward-setting.component.ts
+++ src/app/admin/reward-setting/reward-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { adminRewardSettingService } from 'src/app/Service/Admin/admin-reward-setting.service';
 
 @Component({
   selector: 'app-reward-setting',
@@ -12,16 +13,52 @@ export class RewardSettingComponent {
   rewardno: string = '';
   rewardamount: string = '';
   reward: string = '';
-
+  table: any[] = [];
+  selectedData!: any[];
+  constructor(private adminRewardService: adminRewardSettingService) {
+    this.adminRewardService.showReward().subscribe(
+      (response) => {
+        this.table = response as any[];
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
   add() {
+    if (
+      this.date != '' &&
+      this.time != '' &&
+      this.reward != '' &&
+      this.rewardamount != ''
+    ) {
+      const rewardSetting = {
+        rewardDate: this.date,
+        rewardTitle: this.reward,
+        rewardQuantity: this.rewardamount,
+        rewardTime: this.time,
+      };
+      this.adminRewardService.addReward(rewardSetting).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     this.showTable = true;
   }
   clear() {
     this.showTable = true;
-    this.date = "";
-    this.time = "";
-    this.rewardno = "";
-    this.rewardamount = "";
-    this.reward = "";
+    this.date = '';
+    this.time = '';
+    this.rewardno = '';
+    this.rewardamount = '';
+    this.reward = '';
+  }
+  delete() {
+    console.log(this.selectedData[0].rewardId);
   }
 }
